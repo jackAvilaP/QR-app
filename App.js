9014de493f3896a598806482a8c60c0e7c0b4ada
jackAvilaP <arenca44@gmail.com>
2023-03-31 12:05:40 -0500

    add img styles

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ImageBackground } from 'react-native';
 import CardId from './src/components/CardId';
-
+import ampersand from './src/assets/img/AMPERSAND.png';
 import ScannerQR from './src/components/ScannerQR';
 import Whatsapp from './src/components/Whatsapp';
 export default function App() {
   return (
-    <View style={styles.container}>
+      <ImageBackground source={ampersand} style={styles.container}>
       {/* <ScannerQR /> */}
       {/* <Whatsapp /> */}
-      <CardId/>
+        <CardId/>
+      </ImageBackground>
       
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#ffff',
+    resizeMode: 'cover',
     alignItems: 'center',
+    justifyContent: 'center',
     justifyContent: 'center',
   },
   button:{
